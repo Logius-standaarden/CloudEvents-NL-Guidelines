46ef9e13392c37ae0a15af677777bdf158351bff
Alexander Green <alexander.green@logius.nl>
2022-10-31 13:12:17 +0100

    Syntax error in template

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const documentConfig = {
+var respecConfig = {
     //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
     specStatus: "WV",
     //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
